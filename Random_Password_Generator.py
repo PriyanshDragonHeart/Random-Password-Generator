c092d94eacec39c07e8852f6bee220a5f56c750e
Priyansh Mahajan <priyanshdragonheart@gmail.com>
2020-08-16 19:53:59 +0530

    Add files via upload

---
+++ Random_Password_Generator.py
@@ -0,0 +1,28 @@
+#import random module
+import random
+#initialize a list
+option=[]
+#declare char
+upper='A'
+lower='a'
+#using brute force to enter char into list
+for i in range (0,26):
+    option.append(upper)
+    upper=chr(ord(upper)+1)
+for j in range(0,26):
+    option.append(lower)
+    lower=chr(ord(lower)+1)
+#enter int into list
+num=['0','1','2','3','4','5','6','7','8','9']
+option+=num
+#enter special characters to list
+special_char=['!','@','#','$','%','&']
+option+=special_char
+#ask for length of password
+print("Password Length:")
+password_length=int(input())
+#select random characters and join them
+random_char=[random.choice(option) for i in range(password_length)]
+random_password="".join(random_char)
+#print password
+print (random_password)
